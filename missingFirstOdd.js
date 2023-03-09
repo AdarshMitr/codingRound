eeb3f1b97e934c864b6138f5f60a9e0ba11d894b
AdarshMitr <adarshmitr@gmail.com>
2023-03-09 13:24:42 +0530

    First missing number in an Arithmetic Progression

--- missingFirstOdd.js
+++ missingFirstOdd.js
@@ -1,11 +1,17 @@
 // Find the first missing Odd number in the array.
 
+//for finding first missing number in an Arithmetic Progression
+
 const input=[5,7,9,11,15,17];
+//let n=2;
+const input1=[2,5,8,14,17]
+let n=3;
+
 const output=13;
 function findMissingOdd(input){
     let missingFirstOdd=input[0];
     for(let i=0;i<input.length;i++){
-        let temp=input[i]+2;
+        let temp=input[i]+n;
         if(temp!=input[i+1]){
             missingFirstOdd=temp;
             break;
@@ -14,7 +20,7 @@ function findMissingOdd(input){
     return missingFirstOdd;
     
 }
-console.log(findMissingOdd(input));
 
 
-document.getElementById('show').innerHTML=findMissingOdd(input);
+
+document.getElementById('show').innerHTML=findMissingOdd(input1);
